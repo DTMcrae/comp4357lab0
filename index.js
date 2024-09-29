0574e3745cbf3abb2655aeeac53505f00089de8f
DTMcrae <dtmcrae0@gmail.com>
2024-09-29 14:44:27 -0700

    Updated base routing to support multiple labs.

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use("/js", express.static("./js"));
-app.use("/css", express.static("./css"));
-app.use("/en", express.static("./lang/messages/en"));
 
-// Serve the HTML file
-app.get('/0/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+const lab0 = require("./lab0/route");
+const lab3 = require("./lab3/route");
+
+app.use("/0", lab0);
+app.use("/3", lab3);
 
 // Listen on the defined port
 app.listen(PORT, () => {
