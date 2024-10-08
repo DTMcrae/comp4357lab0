b66d2267f652ba0587b5bb9e18ade6fdb06e33fa
DTMcrae <dtmcrae0@gmail.com>
2024-10-08 09:43:48 -0700

    Fixed a typo in the route creation.

--- lab4/route.js
+++ lab4/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const router = express.router();
+const router = express.Router();
 const PORT = process.env.PORT || 4000;
 
 const dictionary = {};
